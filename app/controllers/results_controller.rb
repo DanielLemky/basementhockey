6673852ddeb5ef570e47fe7527c20afa3260216f
Daniel Lemky <daniel@quantumfirelabs.com>
2021-06-04 01:16:03 -0500

    Refactoring

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -33,54 +33,67 @@ class ResultsController < ApplicationController
             params.permit(:away_goals, :home_goals, :overtime, :shootout, :game_id, :game_photo)
         end
 
-        def update_stats
-            @game = Game.find(params[:game_id])
-            @home_team = User.find(@game.home_team_id)
-            @away_team = User.find(@game.away_team_id)
-
-            if @result.home_goals < @result.away_goals
-                @winner = @away_team
-                @loser = @home_team
+        def winner(result)
+            if result.home_goals < result.away_goals
+                winner = User.find(result.game.away_team_id)
             else
-                @winner = @home_team
-                @loser = @away_team
+                winner = User.find(result.game.home_team_id)
             end
+        end
 
-            @winner_stats = @winner.team_stats.where(season_id: @game.season_id).first
-            @loser_stats = @loser.team_stats.where(season_id: @game.season_id).first
+        def loser(result)
+            if result.home_goals < result.away_goals
+                loser = User.find(result.game.home_team_id)
+            else
+                loser = User.find(result.game.away_team_id)
+            end
+        end
 
-            @winner_stats.wins = @winner_stats.wins + 1
-            @winner_stats.points = @winner_stats.points + 2
+        def add_win_stats(team_stats)
+            team_stats.wins = team_stats.wins + 1
+            team_stats.points = team_stats.points + 2
+            team_stats
+        end
 
+        def add_loss_stats(team_stats)
             if @result.overtime == 1
-                @loser_stats.overtime_losses = @loser_stats.overtime_losses + 1
-                @loser_stats.points = @loser_stats.points + 1
+                team_stats.overtime_losses = team_stats.overtime_losses + 1
+                team_stats.points = team_stats.points + 1
             else
-                @loser_stats.losses = @loser_stats.losses + 1
+                team_stats.losses = team_stats.losses + 1
             end
+            team_stats
+        end
+
+        def update_stats
+            winner_stats = winner(@result).team_stats.find_by(season_id: @game.season_id)
+            loser_stats = loser(@result).team_stats.find_by(season_id: @game.season_id)
+
+            winner_stats = add_win_stats(winner_stats)
+            loser_stats = add_loss_stats(loser_stats)
+
 
             if @result.home_goals < @result.away_goals
                 # away won
-                @winner_stats.goals_for = @winner_stats.goals_for + @result.away_goals
-                @loser_stats.goals_for = @loser_stats.goals_for + @result.home_goals
+                winner_stats.goals_for = winner_stats.goals_for + @result.away_goals
+                loser_stats.goals_for = loser_stats.goals_for + @result.home_goals
 
-                @winner_stats.goals_against = @winner_stats.goals_against + @result.home_goals
-                @loser_stats.goals_against = @loser_stats.goals_against + @result.away_goals
+                winner_stats.goals_against = winner_stats.goals_against + @result.home_goals
+                loser_stats.goals_against = loser_stats.goals_against + @result.away_goals
             else
                 # home won
-                @winner_stats.goals_for = @winner_stats.goals_for + @result.home_goals
-                @loser_stats.goals_for = @loser_stats.goals_for + @result.away_goals
+                winner_stats.goals_for = winner_stats.goals_for + @result.home_goals
+                loser_stats.goals_for = loser_stats.goals_for + @result.away_goals
 
-                @winner_stats.goals_against = @winner_stats.goals_against + @result.away_goals
-                @loser_stats.goals_against = @loser_stats.goals_against + @result.home_goals
+                winner_stats.goals_against = winner_stats.goals_against + @result.away_goals
+                loser_stats.goals_against = loser_stats.goals_against + @result.home_goals
             end
-            
-            @winner_stats.goal_difference = @winner_stats.goals_for - @winner_stats.goals_against
-            @loser_stats.goal_difference = @loser_stats.goals_for - @loser_stats.goals_against
+            winner_stats.goal_difference = winner_stats.goals_for - winner_stats.goals_against
+            loser_stats.goal_difference = loser_stats.goals_for - loser_stats.goals_against
 
 
-            @winner_stats.save
-            @loser_stats.save
+            winner_stats.save
+            loser_stats.save
             
         end
 end
