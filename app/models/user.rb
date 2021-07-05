f556c9d05aa1f2d5ae0f8a4c9a834426478df515
Daniel Lemky <daniel@quantumfirelabs.com>
2021-07-05 00:08:12 -0500

    Added role column to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_many :results, through: :games
   has_many :team_stats
   has_many :game_times, through: :games
+
+  enum role: [:super_admin, :season_admin, :player]
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :player
+  end
 end
